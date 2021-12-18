5dd6b1ac56bf45770d4722de3f8b0014b5ae0640
thanhyou00 <truc06082000@gmail.com>
2021-12-18 16:04:07 +0700

    thanhyou00

---
+++ src/views/ReactStrap.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Badge } from 'reactstrap';
+
+function ReactStrap() {
+    return (
+        <div>
+            <div>
+                <Badge color="primary">
+                    New
+                </Badge>
+            </div>
+        </div>
+    )
+}
+
+export default ReactStrap
